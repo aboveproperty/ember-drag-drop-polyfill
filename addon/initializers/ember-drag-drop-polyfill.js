5694479a910a0219e1b90b8464d18e6dcbe04553
Tyler Bratton <tyler.bratton@aboveproperty.com>
2020-07-06 17:00:46 -0400

    Update iOS workaround

--- addon/initializers/ember-drag-drop-polyfill.js
+++ addon/initializers/ember-drag-drop-polyfill.js
@@ -5,12 +5,33 @@ import { polyfill } from 'mobile-drag-drop';
 import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scroll-behaviour';
 
 export function initialize(/* application */) {
+  let passiveSupported = false;
+
+  try {
+    const opts = {
+      get passive() { // This function will be called when the browser
+        //   attempts to access the passive property.
+        passiveSupported = true;
+        return false;
+      }
+    };
+
+    window.addEventListener("test", null, opts);
+    window.removeEventListener("test", null, opts);
+  } catch (err) {
+    passiveSupported = false;
+  }
 
   let options = config['ember-drag-drop-polyfill'] || {};
 
   if (options.includeScrollBehavior) {
     options.dragImageTranslateOverride = scrollBehaviourDragImageTranslateOverride;
-    window.addEventListener('touchmove', function() {});
+
+    if (passiveSupported) {
+      window.addEventListener('touchmove', function() {}, { passive: false });
+    } else {
+      window.addEventListener('touchmove', function() {});
+    }
   }
 
   polyfill(options);
