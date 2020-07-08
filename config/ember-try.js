ef7104454f309dc26797ab33a5c6b69c96519770
Victor Elci <victor.elci@aboveproperty.com>
2020-07-08 18:54:44 -0400

    configure travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,6 +9,9 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
+
+      useYarn: true,
+
       scenarios: [
         {
           name: 'ember-lts-2.18',
