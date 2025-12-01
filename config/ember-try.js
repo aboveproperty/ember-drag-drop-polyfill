8866b234cdf4c3972ddd98c5b6b3d93e98c74a79
Victor Elci <victor.elci@aboveproperty.com>
2025-12-01 14:16:10 -0500

    Update ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -22,30 +22,30 @@ module.exports = async function () {
           },
         },
       },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
-        },
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
+      // {
+      //   name: 'ember-release',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('release'),
+      //     },
+      //   },
+      // },
+      // {
+      //   name: 'ember-beta',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('beta'),
+      //     },
+      //   },
+      // },
+      // {
+      //   name: 'ember-canary',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('canary'),
+      //     },
+      //   },
+      // },
       {
         name: 'ember-classic',
         env: {
